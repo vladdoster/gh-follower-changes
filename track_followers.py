b43e72478f59544a7c3a60d452658594b790754a
vladislav doster <mvdoster@gmail.com>
2026-01-18 21:05:59 -0600

    Comment out follower retrieval loop
    Comment out the follower retrieval loop for debugging.

--- track_followers.py
+++ track_followers.py
@@ -57,9 +57,9 @@ def fetch_followers(api: GhApi, username: str) -> list[str]:
     all_followers = []
     
     try:
-        for page in paged(api.users.list_followers_for_user, username=username, per_page=30):
-            for follower in page:
-                all_followers.append(follower.login)
+        #for page in paged(api.users.list_followers_for_user, username=username, per_page=30):
+        #    for follower in page:
+       #         all_followers.append(follower.login)
         p = pages(api.users.list_followers_for_user, api.last_page(), username=username).concat()
         logger.info(list(f.login for f in p))
     except Exception as e:
