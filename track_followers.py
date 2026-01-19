60d00df3e122ca5d4a77f6220c6d9fdd89788f81
vladislav doster <mvdoster@gmail.com>
2026-01-18 23:45:46 -0600

    Refactor follower tracking with GhApi integration

--- track_followers.py
+++ track_followers.py
@@ -60,8 +60,12 @@ def fetch_followers(api: GhApi, username: str) -> list[str]:
         #for page in paged(api.users.list_followers_for_user, username=username, per_page=30):
         #    for follower in page:
        #         all_followers.append(follower.login)
+
         gh=GhApi(authenticate=False)
+        gh.debug=print_summary
+        logger.info(list(pages(gh['/users/{owner}/followers'](owner='vladdoster',page=gh.last_page(),per_page=100))))
         p = pages(gh.users.list_followers_for_user(username, gh.last_page())).concat()
+        gh.debug=None
         logger.debug(list(f.login for f in p))
     except Exception as e:
         error_msg = str(e)
